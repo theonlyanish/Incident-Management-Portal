dfb0a32f0b3c67784d3eb05204ec8c7f5c549a6d
Anish <akap0009@student.monash.edu>
2025-02-17 01:06:58 +1100

    aws exports

--- src/aws-exports.ts
+++ src/aws-exports.ts
@@ -1,12 +1,28 @@
 const awsConfig = {
-  Auth: {
-    region: import.meta.env.VITE_AWS_REGION,
-    userPoolId: import.meta.env.VITE_AWS_USER_POOLS_ID,
-    userPoolWebClientId: import.meta.env.VITE_AWS_USER_POOLS_WEB_CLIENT_ID,
+  "aws_project_region": import.meta.env.VITE_AWS_REGION,
+  "aws_cognito_region": import.meta.env.VITE_AWS_COGNITO_REGION,
+  "aws_user_pools_id": import.meta.env.VITE_AWS_USER_POOLS_ID,
+  "aws_user_pools_web_client_id": import.meta.env.VITE_AWS_USER_POOLS_WEB_CLIENT_ID,
+  "aws_appsync_graphqlEndpoint": import.meta.env.VITE_AWS_APPSYNC_GRAPHQL_ENDPOINT,
+  "aws_appsync_region": import.meta.env.VITE_AWS_APPSYNC_REGION,
+  "aws_appsync_authenticationType": import.meta.env.VITE_AWS_APPSYNC_AUTHENTICATION_TYPE,
+  "aws_appsync_apiKey": import.meta.env.VITE_AWS_APPSYNC_API_KEY,
+  "aws_cognito_username_attributes": [],
+  "aws_cognito_social_providers": [],
+  "aws_cognito_signup_attributes": [
+    "EMAIL"
+  ],
+  "aws_cognito_mfa_configuration": "OFF",
+  "aws_cognito_mfa_types": [
+    "SMS"
+  ],
+  "aws_cognito_password_protection_settings": {
+    "passwordPolicyMinLength": 8,
+    "passwordPolicyCharacters": []
   },
-  aws_appsync_graphqlEndpoint: import.meta.env.VITE_AWS_APPSYNC_GRAPHQL_ENDPOINT,
-  aws_appsync_region: import.meta.env.VITE_AWS_APPSYNC_REGION,
-  aws_appsync_authenticationType: import.meta.env.VITE_AWS_APPSYNC_AUTHENTICATION_TYPE,
-};
+  "aws_cognito_verification_mechanisms": [
+    "EMAIL"
+  ]
+} as const;
 
 export default awsConfig; 
